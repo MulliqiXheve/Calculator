30e14b28718a0220f714e70680412f5142fc5f48
Xheve Mulliqi <37428674+MulliqiXheve@users.noreply.github.com>
2018-12-02 14:09:38 -0500

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,191 +1,126 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.util.*;
+package Simplecalc;
 
-public class JavaCalculator extends JFrame {
-    private JButton jbtNum1;
-        private JButton jbtNum2;
-        private JButton jbtNum3;
-        private JButton jbtNum4;
-        private JButton jbtNum5;
-        private JButton jbtNum6;
-        private JButton jbtNum7;
-        private JButton jbtNum8;
-        private JButton jbtNum9;
-        private JButton jbtNum0;
-    private JButton jbtEqual;
-        private JButton jbtAdd;
-        private JButton jbtSubtract;
-        private JButton jbtMultiply;
-        private JButton jbtDivide;
-        private JButton jbtSolve;
-        private JButton jbtClear;
-        private double TEMP;
-        private double SolveTEMP;
-    private JTextField jtfResult;
-    String display = "";
-
-    public JavaCalculator() {
-        JPanel p1 = new JPanel();
-        p1.setLayout(new GridLayout(4, 3));
-        p1.add(jbtNum1 = new JButton("1"));
-        p1.add(jbtNum2 = new JButton("2"));
-        p1.add(jbtNum3 = new JButton("3"));
-        p1.add(jbtNum4 = new JButton("4"));
-        p1.add(jbtNum5 = new JButton("5"));
-        p1.add(jbtNum6 = new JButton("6"));
-        p1.add(jbtNum7 = new JButton("7"));
-        p1.add(jbtNum8 = new JButton("8"));
-        p1.add(jbtNum9 = new JButton("9"));
-        p1.add(jbtNum0 = new JButton("0"));
-        p1.add(jbtClear = new JButton("C"));
-
-
-        JPanel p2 = new JPanel();
-        p2.setLayout(new FlowLayout());
-        p2.add(jtfResult = new JTextField(20));
-        jtfResult.setHorizontalAlignment(JTextField.RIGHT);
-        jtfResult.setEditable(false);
-
-                JPanel p3 = new JPanel();
-                p3.setLayout(new GridLayout(5, 1));
-                p3.add(jbtAdd = new JButton("+"));
-                p3.add(jbtSubtract = new JButton("-"));
-                p3.add(jbtMultiply = new JButton("*"));
-                p3.add(jbtDivide = new JButton("/"));
-                p3.add(jbtSolve = new JButton("="));
-
-        JPanel p = new JPanel();
-        p.setLayout(new GridLayout());
-        p.add(p2, BorderLayout.NORTH);
-        p.add(p1, BorderLayout.SOUTH);
-        p.add(p3, BorderLayout.EAST);
-
-
-        add(p);
-
-        jbtNum1.addActionListener(new ListenToOne());
-        jbtNum2.addActionListener(new ListenToTwo());
-        jbtNum3.addActionListener(new ListenToThree());
-                jbtNum4.addActionListener(new ListenToFour());
-        jbtNum5.addActionListener(new ListenToFive());
-        jbtNum6.addActionListener(new ListenToSix());
-        jbtNum7.addActionListener(new ListenToSeven());
-        jbtNum8.addActionListener(new ListenToEight());
-        jbtNum9.addActionListener(new ListenToNine());
-        jbtNum0.addActionListener(new ListenToZero());
-
-        jbtAdd.addActionListener(new ListenToAdd());
-        jbtSubtract.addActionListener(new ListenToSubtract());
-        jbtMultiply.addActionListener(new ListenToMultiply());
-        jbtDivide.addActionListener(new ListenToDivide());
-        jbtSolve.addActionListener(new ListenToSolve());
-
-
-
-    } //JavaCaluclator()
-
-    class ListenToOne implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "1");
-        }
-    }
-    class ListenToTwo implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "2");
-        }
-    }
-    class ListenToThree implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "3");
-        }
-    }
-    class ListenToFour implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "4");
-        }
-    }
-    class ListenToFive implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "5");
-        }
-    }
-    class ListenToSix implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "6");
-        }
-    }
-    class ListenToSeven implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "7");
-        }
-    }
-    class ListenToEight implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "8");
-        }
-    }
-    class ListenToNine implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "9");
-        }
-    }
-    class ListenToZero implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "0");
-        }
-    }
-
-        class ListenToAdd implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            TEMP = Double.parseDouble(jtfResult.getText());
-                        jtfResult.setText("");
-
-        }
-    }
-    class ListenToSubtract implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "0");
-        }
-    }
-    class ListenToMultiply implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "0");
-        }
-    }
-    class ListenToDivide implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display = jtfResult.getText();
-            jtfResult.setText(display + "0");
-        }
-    }
-    class ListenToSolve implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-                        SolveTEMP = jtfResult.getText();
-            jtfResult.setText(TEMP + Double.parseDouble(jtfResult);
-        }
-    }
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        JavaCalculator calc = new JavaCalculator();
-        calc.pack();
-        calc.setLocationRelativeTo(null);
-                calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        calc.setVisible(true);
-    }
-
+//Imports are listed in full to show what's being used
+//could just import javax.swing.* and java.awt.* etc..
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Container;
+ public class Simplecalc implements ActionListener{
+  JFrame guiFrame;
+  JPanel buttonPanel;
+  JTextField numberCalc;
+  int calcOperation = 0;
+  int currentCalc;
+//Note: Typically the main method will be in a
+//separate class. As this is a simple one class
+//example it's all in the one class.
+public static void main(String[] args) {
+//Use the event dispatch thread for Swing components
+EventQueue.invokeLater(new Runnable()
+{
+@Override
+public void run()
+{
+new Simplecalc();
+}
+});
+}
+public Simplecalc()
+{
+guiFrame = new JFrame();
+//make sure the program exits when the frame closes
+guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+guiFrame.setTitle("Simple Calculator");
+guiFrame.setSize(300,300);
+//This will center the JFrame in the middle of the screen
+guiFrame.setLocationRelativeTo(null);
+numberCalc = new JTextField();
+numberCalc.setHorizontalAlignment(JTextField.RIGHT);
+numberCalc.setEditable(false);
+guiFrame.add(numberCalc, BorderLayout.NORTH);
+buttonPanel = new JPanel();
+//Make a Grid that has three rows and four columns
+buttonPanel.setLayout(new GridLayout(4,3));
+guiFrame.add(buttonPanel, BorderLayout.CENTER);
+//Add the number buttons
+for (int i=1;i<10;i++)
+{
+addButton(buttonPanel, String.valueOf(i));
+}
+JButton addButton = new JButton("+");
+addButton.setActionCommand("+");
+OperatorAction subAction = new OperatorAction(1);
+addButton.addActionListener(subAction);
+JButton subButton = new JButton("-");
+subButton.setActionCommand("-");
+OperatorAction addAction = new OperatorAction(2);
+subButton.addActionListener(addAction);
+JButton equalsButton = new JButton("=");
+equalsButton.setActionCommand("=");
+equalsButton.addActionListener(new ActionListener()
+{
+@Override
+public void actionPerformed(ActionEvent event)
+{
+if (!numberCalc.getText().isEmpty())
+{
+int number = Integer.parseInt(numberCalc.getText());
+if (calcOperation == 1)
+{
+int calculate = currentCalc + number;
+numberCalc.setText(Integer.toString(calculate));
+}
+else if (calcOperation == 2)
+{
+int calculate = currentCalc - number;
+numberCalc.setText(Integer.toString(calculate));
+}
+}
+}
+});
+buttonPanel.add(addButton);
+buttonPanel.add(subButton);
+buttonPanel.add(equalsButton);
+guiFrame.setVisible(true);
+}
+//All the buttons are following the same pattern
+//so create them all in one place.
+private void addButton(Container parent, String name)
+{
+JButton but = new JButton(name);
+but.setActionCommand(name);
+but.addActionListener(this);
+parent.add(but);
+}
+//As all the buttons are doing the same thing it's
+//easier to make the class implement the ActionListener
+//interface and control the button clicks from one place
+@Override
+public void actionPerformed(ActionEvent event)
+{
+//get the Action Command text from the button
+String action = event.getActionCommand();
+//set the text using the Action Command text
+numberCalc.setText(action);
+}
+private class OperatorAction implements ActionListener
+{
+private int operator;
+public OperatorAction(int operation)
+{
+operator = operation;
+}
+public void actionPerformed(ActionEvent event)
+{
+currentCalc = Integer.parseInt(numberCalc.getText());
+calcOperation = operator;
+}
+}
 }
